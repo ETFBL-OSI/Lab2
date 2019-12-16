471a180260c30d05aec8036bcba33d2bef729808
etfstudent1 <etf@mail.com>
2019-12-16 11:45:37 +0100

    korijen_broja

--- main.c
+++ main.c
@@ -1,6 +1,19 @@
+#include<stdio.h>
+#include<math.h>
+
 int stepenovanje(int x)
 {
     int y;
     y=x*x;
     return y;
+}
+double korijen_broja(int x)
+{
+    x=sqrt(x);
+    return x;
+}
+int main()
+{
+
+
 }
