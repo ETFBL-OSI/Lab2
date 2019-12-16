508d03382a5fa1f3c96eaa879dce050e4b0f4302
ETF Student 2 <etf2@mail.com>
2019-12-16 12:01:48 +0100

    Sabiranje

--- main.c
+++ main.c
@@ -35,6 +35,16 @@ long factorial(int n)
 
   return r;
 }
+double sum(int n)
+{
+double s=0, d;
+for(int i=0;i<n;i++)
+{
+printf("Unesite %d. broj, i+1);
+scanf("%lf", &d);
+sum+=d;
+}
+return sum;
 int main()
 {
 
