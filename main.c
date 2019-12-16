92453e7e50ccb0b6800c64ed37ea0cc0c0d0fb6d
etfstudent1 <etf@mail.com>
2019-12-16 11:47:57 +0100

    Sve funkcije u main.c

--- main.c
+++ main.c
@@ -12,6 +12,19 @@ double korijen_broja(int x)
     x=sqrt(x);
     return x;
 }
+double dijelenje(double p, double d)
+{
+    return p/d;
+}
+double oduzimanje(double a, double b)
+{
+	return (a-b);
+}
+
+double sabiranje(double a, double b)
+{
+	return (a+b);
+	}
 int main()
 {
 
