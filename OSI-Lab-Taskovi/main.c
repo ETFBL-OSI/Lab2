d02638f382bf44b4cbca6e87687ffb9cff5759ff
PC08\Student <student@etfbl.net>
2024-11-11 13:31:44 +0100

    Oduzimanje n brojeva

--- OSI-Lab-Taskovi/main.c
+++ OSI-Lab-Taskovi/main.c
@@ -1,8 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void oduzimanje_n_brojeva()
+{
+	int n;
+	printf("Koliko brojeva ima u razlici? ");
+	scanf("%d",&n);
+	int niz[n];
+	printf("Unesite broj od kojeg oduzimamo: ");
+	scanf("%d",&niz[0]);
+	int razlika = niz[0];
+	for(int i = 1; i < n; i++)
+	{
+		printf("Unesite %d. oduzimac: ",i);
+		scanf("%d",&niz[i]);
+		razlika -= niz[i];
+	}
+	printf("Razlika brojeva je: %d\n",razlika);
+}
+
 int main()
 {
-    printf("Hello world!\n");
-    return 0;
+	oduzimanje_n_brojeva();
+	return 0;
 }
