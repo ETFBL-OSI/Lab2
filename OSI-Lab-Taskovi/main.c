101c7c07b4cb14a4980b280e4630aa453f369a0e
PC13\Student <student.etfbl.net>
2024-11-11 13:27:40 +0100

    Oduzimanje 2 broja

--- OSI-Lab-Taskovi/main.c
+++ OSI-Lab-Taskovi/main.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+int oduzimanje2broja(int umanjenik,int umanjilac)
+{
+    return umanjenik-umanjilac;
+}
 int main()
 {
-    printf("Hello world!\n");
+    printf("5-3=%d\n",oduzimanje2broja(5,3));
     return 0;
 }
