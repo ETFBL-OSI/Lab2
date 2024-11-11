def44eb0cca7ee1d426cc0b6f36f352753abdc0d
PC11\Student <student@etfbl.net>
2024-11-11 13:29:20 +0100

    gotovo sabiranje n brojeva

--- OSI-Lab-Taskovi/main.c
+++ OSI-Lab-Taskovi/main.c
@@ -1,8 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int sabiranjeNBr(int n);
+
 int main()
 {
     printf("Hello world!\n");
+	
+	int suma = sabiranjeNBr(5);
+	
     return 0;
+}
+
+int sabiranjeNBr(int n)
+{
+	int suma = 0;
+	int p;
+	
+	printf("Unesite brojeve:");
+	for(int i = 0; i < n; i++)
+	{
+		printf("\n[%d] ", suma);
+		scanf("%d", &p);
+		suma += p;
+	}
+	
+	printf("\nKonacno [%d] ", suma);
+	
+	return suma;
 }
